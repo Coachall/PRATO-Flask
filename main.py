16c4cc6f532d6b9a4035bcb2cd2372c6d1edd731
Jesse <jessesampermans@coachall.be>
2023-11-29 12:56:57 +0100

    gsm

--- main.py
+++ main.py
@@ -213,7 +213,7 @@ def my_task(request_data):
 
                     print(gsm)
 
-                    if math.isnan(gsm) and isinstance(gsm, float):
+                    if math.isnan(gsm):
                         gsm = None  # Set gsm to None or handle it as needed for NaN values
                     else:
                         gsm = str(gsm)
